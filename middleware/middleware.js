d4af84896f0f0a49277e4d84def6766bb14cd3bf
alex-ohdz <alejandrooramash@gmail.com>
2024-01-24 20:40:13 -0500

    oka1234

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,3 +1,15 @@
-export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
 
-export const config = { matcher: ["/carrito"] };
+// Esta función se ejecuta en cada solicitud
+export async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  const pathname = req.nextUrl.pathname;
+
+  // Si no hay token y se está intentando acceder a una ruta protegida
+  if (!token && pathname.startsWith('/whishlist')) {
+    const url = req.nextUrl.clone();
+    url.pathname = '/'; // Redirige a la página de inicio de sesión
+    return Response.redirect(url);
+  }
+}
